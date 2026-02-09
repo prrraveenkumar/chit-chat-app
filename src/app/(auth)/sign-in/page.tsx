b2751ecda6f12f0f725ffd99e984da1febcefed9
prrraveenkumar <pk248539@gmail.com>
2026-02-09 22:08:08 +0530

    feat: update dependencies and add new UI components
    - Added new dependencies including @hookform/resolvers, axios, class-variance-authority, clsx, lucide-react, next-themes, radix-ui, react-hook-form, sonner, tailwind-merge, and usehooks-ts.
    - Updated globals.css to include new styles and custom properties for dark mode and theme variants.
    - Implemented a new sign-in page with form validation using Zod and react-hook-form.
    - Created reusable UI components for buttons, forms, inputs, labels, and notifications using Radix UI and Tailwind CSS.
    - Introduced a Toaster component for displaying notifications with customizable themes.
    - Added utility functions for class name management.

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,157 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from 'zod'
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schema/signUpSchema"
+import { signInSchema } from "@/schema/signInSchema"
+import axios, { AxiosError } from 'axios'
+import { ApiResponse } from "@/types/apiResponse"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+
+function page() {
+    const [username, setUsername] = useState('')
+    const [usernameMessage, setUsernameMessage] = useState('')
+    const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const debouncedUsername = useDebounceValue(username, 500)
+    const router = useRouter()
+
+
+
+    // zod implementation
+
+    const form = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            username: '',
+            email: '',
+            password: ''
+        }
+    })
+
+    useEffect(() => {
+        const checkUsernameUnique = async () => {
+            if (debouncedUsername) {
+                setIsCheckingUsername(true)
+                setUsernameMessage('')
+                try {
+                    const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername})`)
+                    setUsernameMessage(response.data.message)
+                } catch (error) {
+                    const axiosError = error as AxiosError<ApiResponse>
+                    setUsernameMessage(axiosError.response?.data.message ?? "Error checking username")
+
+                } finally {
+                    setIsCheckingUsername(false)
+                }
+            }
+        }
+
+        checkUsernameUnique()
+    },
+        [debouncedUsername])
+
+    const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+        setIsSubmitting(true)
+        try {
+            const response = await axios.post<ApiResponse>('api/signup', data)
+            toast("success", {
+                description: response?.data.message
+            })
+            router.replace(`/verify/${username}`)
+            setIsSubmitting(false)
+        } catch (error) {
+            console.error("Error in signup of user", error)
+            const axiosError = error as AxiosError<ApiResponse>
+            let errorMessage = axiosError.response?.data.message
+            toast("Signup failed", {
+                description: errorMessage,
+            })
+            setIsSubmitting(false)
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6"> Join Mystery Messasge</h1>
+                    <p className="mb-4">
+                        Sign up to start your anonymous adventure
+                    </p>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={
+                                ({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your username" {...field} onChange={(e) => {
+                                                field.onChange(e)
+                                                setUsername(e.target.value)
+                                            }} />
+                                        </FormControl>
+
+                                        <FormMessage />
+                                    </FormItem>
+                                )
+                            } />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={
+                                ({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your email" {...field} />
+                                        </FormControl>
+
+                                        <FormMessage />
+                                    </FormItem>
+                                )
+                            } />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={
+                                ({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input type="password" placeholder="Enter your password" {...field} />
+                                        </FormControl>
+
+                                        <FormMessage />
+                                    </FormItem>
+                                )
+                            } />
+
+
+                        <Button type="submit" disabled={isSubmitting}>
+                            {isSubmitting ? ( <><Loader2 className="animate-spin mr-2 h-4  w-4"  /> Please wait...</>): ("Sign up")}
+                        </Button>
+                    </form>
+                </Form>
+                <div>
+                    Already have an account? {''}
+                    <Link href = '/sign-in' className="text-blue-500 hover:text-blue-900">Sign in</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
